1044ad14e53cbe9033a1e19328371152907f14c1
ngovanphat <1753082@student.hcmus.edu.vn>
2020-12-17 11:32:01 +0700

    a little change about login page

---
+++ src/components/Login/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+import {  makeStyles } from '@material-ui/core/styles';
+
+
+function Login(){
+    return (
+        <Container maxWidth="sm">
+            <Typography>Login</Typography>
+        </Container>
+    );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Login from './components/Login';
 import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
